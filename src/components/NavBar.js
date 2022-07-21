79f53c7a22b8a506b3956fa9808393aa8e2189eb
gmmastra <gmmastra@ncsu.edu>
2022-07-21 16:12:45 -0400

    Improvements to Home, added menu to Artwork

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,73 +39,77 @@ export function NavBar(props) {
     }
 
     return (
-        <AppBar position="fixed" className="body theme-force" elevation={0}>
-            <Slide direction="down" in={true} timeout={500} mountOnEnter unmountOnExit>
-                <Box style={{ margin: '10px 30px 0 30px' }}>
+        <div>
+            <div className="border" />
+            <AppBar position="fixed" className="body theme-force" elevation={0}>
+                <Slide direction="down" in={true} timeout={500} mountOnEnter unmountOnExit>
+                    <Box style={{ margin: '10px 30px 0 30px' }}>
 
-                    {/* Mobile Menu */}
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                        <Button onClick={toggleDrawer(true)}><MenuIcon className="theme-element" /></Button>
-                        <SwipeableDrawer open={state} onClose={toggleDrawer(false)} classes={{ paper: "theme-force drawer" }}>
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/" className="drawer-item">
-                                <span>HOME</span>
-                            </Button>
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/artwork" className="drawer-item">
-                                <span>ARTWORK</span>
-                            </Button>
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/webdev" className="drawer-item">
-                                <span>WEB DEVELOPMENT</span>
-                            </Button>
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
-                                <span>GAME DEVELOPMENT</span>
-                            </Button>
-                            <Divider sx={{ m: 1 }} />
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
-                                <span>RESUME</span>
-                            </Button>
-                            <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
-                                <span>CONTACT</span>
-                            </Button>
-                            <Divider sx={{ m: 1 }} />
-                            <Box className="theme-force link" style={{ padding: '0 10px 110px 10px', cursor: 'pointer', marginTop: 'auto' }}>
-                                {props.theme === 'dark' && <DarkModeIcon onClick={changeMode} />}
-                                {props.theme === 'light' && <LightModeIcon onClick={changeMode} />}
-                            </Box>
-                        </SwipeableDrawer>
-                    </Box>
+                        {/* Mobile Menu */}
+                        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                            <Button onClick={toggleDrawer(true)}><MenuIcon className="theme-element" /></Button>
+                            <SwipeableDrawer open={state} onClose={toggleDrawer(false)} classes={{ paper: "theme-force drawer" }}>
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/" className="drawer-item">
+                                    <span>HOME</span>
+                                </Button>
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/artwork" className="drawer-item">
+                                    <span>ARTWORK</span>
+                                </Button>
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/webdev" className="drawer-item">
+                                    <span>WEB DEVELOPMENT</span>
+                                </Button>
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
+                                    <span>GAME DEVELOPMENT</span>
+                                </Button>
+                                <Divider sx={{ m: 1 }} />
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
+                                    <span>RESUMÉ</span>
+                                </Button>
+                                <Button onClick={toggleDrawer(false)} component={Link} to="/gamedev" className="drawer-item">
+                                    <span>CONTACT</span>
+                                </Button>
+                                <Divider sx={{ m: 1 }} />
+                                <Box className="theme-force link" style={{ padding: '0 10px 110px 10px', cursor: 'pointer', marginTop: 'auto' }}>
+                                    {props.theme === 'dark' && <DarkModeIcon onClick={changeMode} />}
+                                    {props.theme === 'light' && <LightModeIcon onClick={changeMode} />}
+                                </Box>
+                            </SwipeableDrawer>
+                        </Box>
 
-                    {/* Desktop Menu */}
-                    <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            <Box component={Link} to="/" sx={{ m: 1, textDecoration: 'none' }} className="theme-element link">
-                                <BubbleChartIcon style={{width: '30px'}} />
+                        {/* Desktop Menu */}
+                        <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', justifyContent: 'space-between' }}>
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                <Box component={Link} to="/" sx={{ m: 1, textDecoration: 'none' }} className="theme-element link">
+                                    <BubbleChartIcon style={{ width: '30px' }} />
+                                </Box>
+                                <Box component={Link} to="/artwork" sx={{ m: 1, textDecoration: 'none' }}>
+                                    <span className="link">ARTWORK</span>
+                                </Box>
+                                <Box component={Link} to="/webdev" sx={{ m: 1, textDecoration: 'none' }}>
+                                    <span className="link">WEB DEVELOPMENT</span>
+                                </Box>
+                                <Box component={Link} to="/gamedev" sx={{ m: 1, textDecoration: 'none' }}>
+                                    <span className="link">GAME DEVELOPMENT</span>
+                                </Box>
                             </Box>
-                            <Box component={Link} to="/artwork" sx={{ m: 1, textDecoration: 'none' }}>
-                                <span className="link">ARTWORK</span>
-                            </Box>
-                            <Box component={Link} to="/webdev" sx={{ m: 1, textDecoration: 'none' }}>
-                                <span className="link">WEB DEVELOPMENT</span>
-                            </Box>
-                            <Box component={Link} to="/gamedev" sx={{ m: 1, textDecoration: 'none' }}>
-                                <span className="link">GAME DEVELOPMENT</span>
+
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                {props.theme === 'light' && <LightModeIcon onClick={changeMode} className="theme-element link" style={{ margin: '10px', cursor: 'pointer' }} />}
+                                {props.theme === 'dark' && <DarkModeIcon onClick={changeMode} className="theme-element link" style={{ margin: '10px', cursor: 'pointer' }} />}
+
+                                <Box component={Link} to="/#" sx={{ m: 1, textDecoration: 'none' }}>
+                                    <span className="link">RESUMÉ</span>
+                                </Box>
+                                <Box component={Link} to="/#" sx={{ m: 1, textDecoration: 'none' }}>
+                                    <span className="link">CONTACT</span>
+                                </Box>
                             </Box>
                         </Box>
 
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            {props.theme === 'light' && <LightModeIcon onClick={changeMode} className="theme-element link" style={{ margin: '10px', cursor: 'pointer' }} />}
-                            {props.theme === 'dark' && <DarkModeIcon onClick={changeMode} className="theme-element link" style={{ margin: '10px', cursor: 'pointer' }} />}
-
-                            <Box component={Link} to="/#" sx={{ m: 1, textDecoration: 'none' }}>
-                                <span className="link">RESUME</span>
-                            </Box>
-                            <Box component={Link} to="/#" sx={{ m: 1, textDecoration: 'none' }}>
-                                <span className="link">CONTACT</span>
-                            </Box>
-                        </Box>
                     </Box>
-
-                </Box>
-            </Slide>
-        </AppBar>
+                </Slide>
+            </AppBar>
+            <div className="border-reverse" />
+        </div>
     );
 }
--- src/pages/Artwork.js
+++ src/pages/Artwork.js
@@ -1,13 +1,111 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { styled } from "@mui/material/styles";
+import { Box, Tab, Tabs, Typography } from '@mui/material';
 import Slide from '@mui/material/Slide';
+
+{/* Navigation tabs */ }
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box style={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+{/* Custom tabs */ }
+const StyledTab = styled((props) => (
+    <Tab disableRipple {...props} />
+))(({ }) => ({
+    marginRight: '-10px',
+    marginLeft: '5px',
+    display: 'flex',
+    justifyContent: 'center',
+    border: "1px solid transparent",
+    borderRadius: "0.25rem",
+    color: 'var(--text-primary)',
+    fontFamily: 'Cinzel, serif !important',
+    transition: `color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out`,
+    "&.Mui-selected": {
+        color: 'var(--text-primary)',
+        borderColor: 'var(--text-primary)',
+    }
+}));
+
 
 export function Artwork(props) {
 
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const mediumViewport = useMediaQuery('(min-width:900px)');
+
     return (
-        <Slide direction="down" in={true} mountOnEnter unmountOnExit>
-        <div>
-            <p> Artwork </p>
-        </div>
-        </Slide >
+        <div style={{ display: 'flex' }} class="theme-force">
+
+            {/* Desktop Menu */}
+            <Box class="sidebar">
+                <Box sx={{ width: { xs: '140vw', md: 'auto' }, display: 'flex', justifyContent: 'center' }}>
+                    <Slide direction="right" in={true} mountOnEnter unmountOnExit>
+                        <Tabs
+                            orientation={mediumViewport ? "vertical" : "horizontal"}
+                            variant="scrollable"
+                            value={value}
+                            onChange={handleChange}
+                            TabIndicatorProps={{
+                                style: {
+                                    display: "none",
+                                },
+                            }}
+                            sx={{ borderRight: { xs: 0, md: 1 }, borderBottom: { xs: 1, md: 0 }, padding: { xs: '20px 0px 0px 0px', md: '50px 0px 0px 10px' }, height: { xs: 0, md: '80vh' } }}>
+                            <StyledTab label="GALLERY" {...a11yProps(0)} />
+                            <StyledTab label="COMMISSIONS" {...a11yProps(1)} />
+                            <StyledTab label="ESJ" {...a11yProps(2)} />
+                            <StyledTab label="ARTFIGHT" {...a11yProps(3)} />
+                        </Tabs>
+                    </Slide>
+                </Box>
+
+                {/* Tab contents */}
+                <Box style={{ flexGrow: 1 }} >
+                    <TabPanel value={value} index={0} class="theme-force sidebar-item">
+                        <h2> GALLERY </h2>
+                    </TabPanel>
+                    <TabPanel value={value} index={1} class="theme-force sidebar-item">
+                        <h2> COMMISSIONS </h2>
+                    </TabPanel>
+                    <TabPanel value={value} index={2} class="theme-force sidebar-item">
+                        <h2> ELEMENTARY STEM JOURNAL </h2>
+                    </TabPanel>
+                    <TabPanel value={value} index={3} class="theme-force sidebar-item">
+                        <h2> ARTFIGHT </h2>
+                    </TabPanel>
+                </Box>
+
+            </Box>
+
+        </div >
     );
 }
